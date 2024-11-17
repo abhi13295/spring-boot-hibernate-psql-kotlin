7033c7da39f5264d8198ee6b91815f4ac1636edc
Abhinav <abhinav@MAC-LEAPLAP1557.local>
2024-11-17 16:17:01 +0530

    added classes and kotlin dependency

--- src/main/java/com/outleap/demo/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.outleap.demo.repository;
-
-import com.outleap.demo.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-
-
-}
